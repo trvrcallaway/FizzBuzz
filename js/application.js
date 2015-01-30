c6169dec8ff10a46c9104b6e6b6aa9b71e876d86
trvrcallaway <trevor@stjoeweb.com>
2015-01-30 15:58:56 -0600

    Jan 30

--- js/application.js
+++ js/application.js
@@ -1,11 +1,29 @@
-for (i = 1; i <= 100; i++) {
-  if (i % 15 == 0) {
-    console.log("FizzBuzz");
-} else if (i % 3 == 0) {
-    console.log("Fizz");
-} else if (i % 5 == 0) {
-    console.log("Buzz");
+$(document).ready(function() {
+
+var theNumber = Number(prompt("Please enter a number"));
+var element = document.getElementById("fizzer");
+
+if(theNumber){
+  element.innerHTML = FizzBuzz(Number);
 } else {
-        console.log(i);
+  element.innerHTML = 'no answer provided';
+  };
+});
+
+var FizzBuzz = function(Number) {
+  for (i = 1; i <= Number; i++) {
+    if (i % 15 == 0) {
+      $('div').append("Fizzbuzz");
+    }
+
+    else if(i % 3 == 0) {
+      $('div').append("Fizz");
+    }
+    else if(i % 5 == 0) {
+      $('div').append("Buzz");
+    }
+    else {
+      $('div').append(i);
+    }
   }
 }
